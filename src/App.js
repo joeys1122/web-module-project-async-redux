a04b6b17bcd097a4f9a5874faf7922111c49839f
joeys1122 <josephs1122@gmail.com>
2022-01-19 14:39:40 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+
+import Quote from './components/Quote';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Async Redux Project
+      <h1>Random Quote Generator</h1>
+      <span>Built using <a href='https://github.com/fisenkodv/dictum'>Dictum API</a></span>
+      <Quote/>
     </div>
   );
 }
---
+++ src/components/Quote.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Quote() {
+  return (
+    <div className="Quote">
+      Quote Here
+    </div>
+  );
+}
+
+export default Quote;
